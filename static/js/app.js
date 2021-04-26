adfde4f581a56215354e65b60bf63ba15719b294
Tommy Munson <Tmunson50@MacBook-Air.attlocal.net>
2021-04-26 18:44:43 -0500

    more commits

--- static/js/app.js
+++ static/js/app.js
@@ -1,101 +1,12 @@
-// Load CSV Data
 
-_csv = "static/data/data.csv"
+// Set svg Width and Height
+var svgWidth = outerWidth *.75;
+var svgHeight = outerHeight * .75;
 
-// Build Chart Parameters
-
-var svgWidth = 800;
-var svgHeight = 500;
-
+// Set margin parameters
 var margin = {
-
-    top: 20,
-    right: 40,
-    bottom: 60,
-    left: 50
+  top: 20,
+  right: 40,
+  bottom: 60,
+  left: 100
 };
-
-
-var width = svgWidth - margin.left - margin.right;
-var height = svgHeigh - margin.top - margin.bottom;
-
-// Create the SVG Rapper 
-
-var svg = d3
-
-.select("#scatter")
-.append("svg")
-.attr("width",svgWidth)
-.attr("height",svgHeight);
-
-var chartGroup = svg.append("g")
-.attr("transform",'translate(${margin.left},${margin.top');
-
-// Load Data 
-
-d3.csv(_csv).then(function(CensusData)) {
-    Census.Data.forEach(function(data) {
-        data.poverty = +data.poverty;
-        data.healthcare = +data.healthcare;
-    });
-
-
-// Creates the Scale
-
-const xScale = d3.scaleLinear()
-.domain(d3.extent(CensusData,d=> d.poverty))
-.range([0,width])
-.nice();
-
-const yScale = d3.scaleLinear()
-.domain(d3.extent(CensusData, d=> d.healthcare))
-.range([height, 0])
-.nice();
-
-// The Axes
-
-const xAxis = d3.axisBottom(xScale);
-const yAxis = d3.axisLeft(yScale);
-
-// Append Axes to Chartgroup
-chartGroup.append("g").attr("transform", 'translate(0, ${height})').call(xAxis);
-chartGroup.append("g").call(yAxis);
-
-
-chartGroup.selectAll("circle")
-.data(CensusData)
-.enter()
-.append("circle")
-.attr("cx",d=>xScale(d.poverty))
-.attr("cy",d=>yScale(d.healthcare))
-.attr("r","11")
-.classed("stateCircle", true)
-.attr("opactity", 0.75);
-
-
-chartGroup.append("g")
-.selectAll('text')
-.data(CensusData)
-.enter()
-.append("text")
-.text(d=>d.abbr)
-.attr("x",d=>xScale(d.poverty))
-.attr("y",d=>yScale(d.healthcare))
-.classed("stateText", true);
-
-//============add axes titles=========
-chartGroup.append("text")
-.attr("transform", `translate(${width/2}, ${height + margin.top + 20})`)
-.classed("axis", true)
-.text("In Poverty (%)");
-
-chartGroup.append("text")
-.attr("y", 0 - ((margin.left / 2) + 15))
-.attr("x", 0 - (height / 2))
-.classed("axis", true)
-.attr("transform", "rotate(-90)")
-.text("Lacks Healthcare (%)");
-}).catch(function(error) {
-console.log(error);
-
-});
