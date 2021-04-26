29ba3cbc1e18ebc30ce52106b85769b788e70b95
Tommy Munson <Tmunson50@MacBook-Air.local>
2021-04-25 20:16:15 -0500

    Hackathon updates

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,58 @@
-// @TODO: YOUR CODE HERE!
+// Load CSV Data
+
+_csv = "static/data/data.csv"
+
+// Build Chart Parameters
+
+var svgWidth = 800;
+var svgHeight = 500;
+
+var margin = {
+
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 50
+};
+
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeigh - margin.top - margin.bottom;
+
+// Create the SVG Rapper 
+
+var svg = d3
+
+.select("#scatter")
+.append("svg")
+.attr("width",svgWidth)
+.attr("height",svgHeight);
+
+var chartGroup = svg.append("g")
+.attr("transform",'translate(${margin.left},${margin.top');
+
+// Load Data 
+
+d3.csv(_csv).then(function(CensusData)) {
+    Census.Data.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
+    });
+
+
+// Creates the Scale
+
+const xScale = d3.scaleLinear()
+.domain(d3.extent(CensusData,d=> d.poverty))
+.range([0,width])
+.nice();
+
+const yScale = d3.scaleLinear()
+.domain(d3.extent(CensusData, d=> d.healthcare))
+.range([height, 0])
+.nice();
+
+// The Axes
+
+const xAxis = d3.axisBottom(xScale)
+}
